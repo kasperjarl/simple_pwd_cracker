3e727646a0aa340d3480936b4fbf28f2182c7437
kasperjarl <kasper.jarlgren@gmail.com>
2025-01-14 10:42:55 +0100

    added run time and iterations display in terminal

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+from datetime import datetime
+
+
+def main():
+    start_time = datetime.now()
+    i = 0
+    while i < 1_820_028:
+        time_running = (datetime.now()-start_time) 
+        time_running = str(time_running).split(".")[0]
+        i += 1
+        print(f"Run time: {time_running} | iterations: {i:,}", end='\r')
+
+    print(f"Total run time: {time_running} | total iterations: {i:,}.")
+
+if __name__ == "__main__":
+    main()
