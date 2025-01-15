0c8c955c1c1a1a94b18222f2c00dc580406770d8
kasperjarl <kasper.jarlgren@gmail.com>
2025-01-15 15:21:42 +0100

    put the run timer in its own file as a class instead - why? just do practice

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 from datetime import datetime
-
+from run_timer import RunTime
 
 def main():
-    start_time = datetime.now()
-    i = 0
-    while i < 1_820_028:
-        time_running = (datetime.now()-start_time) 
-        time_running = str(time_running).split(".")[0]
-        i += 1
-        print(f"Run time: {time_running} | iterations: {i:,}", end='\r')
-
-    print(f"Total run time: {time_running} | total iterations: {i:,}.")
+    timer = RunTime()
+    timer.run_timer()
 
 if __name__ == "__main__":
     main()
